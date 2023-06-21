6423cf9ec5374874537446868674f82691da7a45
googlebaba01 <130752795+googlebaba01@users.noreply.github.com>
2023-06-21 21:38:20 +0530

    MY INTRO website

---
+++ app.js
@@ -0,0 +1,79 @@
+var wip= document.getElementsByClassName("wip");
+
+var alertwip= function(){
+    alert("Work In Progress!\nBest Wishes\nAnkit");
+}
+
+for(var i=0;i<wip.length;i++) {
+    console.log("Check");
+    wip[i].addEventListener('click',alertwip,false);
+}
+
+
+
+
+// Jquery for button scrolls
+$(document).ready(function(){
+    $('a[href^="#"]').on('click',function (e) {
+        // e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});
+
+
+// For skills animation
+var tl = new TimelineMax({onUpdate:updatePercentage});
+var tl2 = new TimelineMax();
+const controller = new ScrollMagic.Controller();
+
+tl.from(".scroll-anim-span ",0.5,{width:0});
+
+
+const scene = new ScrollMagic.Scene({
+  triggerElement: ".skill-activate",
+            triggerHook: "onLeave",
+            duration: "50%",
+})
+  .setPin(".skills-anim-span")
+  .setTween(tl)
+		.addTo(controller);
+
+
+
+function updatePercentage() {
+  //percent.innerHTML = (tl.progress() *100 ).toFixed();
+  tl.progress();
+  console.log(tl.progress());
+}
+
+
+
+
+//  Nav Bar
+
+const hamburger=document.querySelector(".hamburger");
+const lines=document.querySelectorAll(".lines");
+const nav_ul=document.querySelector("#navbar ul");
+const links=document.querySelector("#navbar ul li");
+
+hamburger.addEventListener('click',(e)=>{
+    nav_ul.classList.toggle('open');
+    
+    for(i=0;i<3;i++){
+        console.log(lines[i]);
+        lines[i].classList.toggle('lines-change');
+    }
+})
+
+links.addEventListener('click',e=>{
+    nav_ul.classList.toggle('open');
+})
